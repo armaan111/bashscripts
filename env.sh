a748d6be078d3ac022c08cdc95a70ea78f21401b
root <root@0469787cc11c.mylabserver.com>
2020-04-25 16:18:43 +0000

    adding  env.sh

---
+++ env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+echo  "This  script  will  give  us  environment  information"
+echo  "===================================================="
+
+echo  ""  
+echo  "Hello  username:  $USER"
+echo  ""
+echo  "Your  Home  directory  is:  $HOME"
+echo  ""
+echo  "Your  History  file  will   ignore:  $HISTCONTROL"
+echo  ""
+echo  "Your  terminal  session  type  is:  $TERM"
+echo  ""
